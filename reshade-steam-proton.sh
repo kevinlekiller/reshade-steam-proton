3904d9b124dbdcb840eabf9f73e7320f600d1647
kevinlekiller <kevinlekiller@users.noreply.github.com>
2021-04-30 10:48:31 -0400

    Refactor, Uninstall option, Automatic DLL check

--- reshade-steam-proton.sh
+++ reshade-steam-proton.sh
@@ -22,68 +22,194 @@ LICENSE
     By linking, we can re-run this script and all the games automatically get the newest ReShade and
     shader versions.
     
-    We can use this script to globally update ReShade and the shaders for all games (press Control+C when you get
-    to the part that asks you for the steamid), we can also use it to install ReShade for a specific game, this
-    will also make sure ReShade and the shaders are up to date.
+    Enviroment Variables:
+        UPDATE_RESHADE
+            To skip checking for ReShade / shader updates, set UPDATE_RESHADE=0
     
-    To skip checking for ReShade / shader updates, set UPDATE_RESHADE=0
+        D3DCOMPILER
+            To skip installing d3dcompiler_47, set D3DCOMPILER=0
     
-    To skip installing d3dcompiler_47, set D3DCOMPILER=0
+        MAIN_PATH
+            By default, ReShade / shader files are stored in ~/.reshade
+            You can override this by setting the MAIN_PATH variable, for example: MAIN_PATH=~/Documents/reshade ./reshade-steam-proton.sh
     
-    To use a custom dll override (instead of dxgi.dll) set CUSTOM_OVERRIDE=
-    This will link dxgi.dll to the game using a different name, for example opengl32.dll and set a wine override for it.
-    Example on a opengl game (Like Wolfenstein: The New Order): CUSTOM_OVERRIDE=opengl32 ./reshade-steam-proton.sh
+    Reuirements:
+        grep
+        curl
+        7z
+        wget
+        protontricks
     
-    By default, files are downloaded to ~/.reshade
-    You can override this by setting the MAIN_PATH variable, for example: MAIN_PATH=~/Documents/reshade ./reshade-steam-proton.sh
+    Notes:
+        Overriding and installing the d3dcompiler_47 dll seems to occasionally fail with proton-ge under protontricks, switch
+        to Steam's proton before running, you can switch back to proton-ge after.
     
-    Requires grep, curl, 7z, wget, protontricks.
-    
-    NOTE: Overriding and installing the d3dcompiler_47 dll seems to occasionally fail with proton-ge under protontricks, switch
-    to Steam's proton before running, you can switch back to proton-ge after.
-    
-    Example usage:
-    
-    If we want to install ReShade on Back To The Future Episode 1:
-    
-    First we run the game, so steam can create the various required directories. Exit the game.
-    
-    Run this command to find the steamid: protontricks -s Back To The Future
-    We then get the name / steamid: Back to the Future: Ep 1 - It's About Time (31290)
-    Then we run this script, when it asks for the steamid, give it 31290,
-    this will install d3dcompiler_47.dll using protontricks,
-    it will also try to change the DLL overrides for dxgi / d3d9 by editing the user.reg file,
-    if automatically editing the user.reg fails, you will instructed how to do this process manually.
-    
-    Then we must find the directory which contains the exe file for the game, you can find it using the Steam client by
-    right clicking the game, click Properties, click Local Files, clicking Browse, or by finding
-    it using the command line: find ~/.local -iname 'Back to the future*'
-    Then we get /home/kevin/.local/share/Steam/steamapps/common/Back to the Future Ep 1
-    Sometimes the exe file is not in the main folder, you might have to look in subdirectories.
-    
-    Finally we are asked to run the game, set the paths to the folders in the ReShade settings for the Effects / Textures.
+    Usage:
+        Installing ReShade for a game:
+            Example on Back To The Future Episode 1:
+            
+                If the game has never been run, run it, steam will create the various required directories.
+                Exit the game.
+                
+                Find the SteamID: protontricks -s Back To The Future
+                    Back to the Future: Ep 1 - It's About Time (31290)
+                
+                Run this script.
+                
+                Type i to install ReShade.
+                
+                Find the game directory where the .exe file is.
+                    You can open the Steam client, right click the game, click Properties,
+                    click Local Files, clicking Browse, find the directory with the main
+                    exe file, copy it, supply it to the script.
+                    
+                    Or you can run : find ~/.local -iname 'Back to the future*'
+                    Then run : ls "/home/kevin/.local/share/Steam/steamapps/common/Back to the Future Ep 1"
+                    We see BackToTheFuture101.exe is in this directory.
+                
+                Select if you want it to automatically detect the correct dll file for ReShade or
+                to manually specity it.
+                
+                Give it 31290 when asked for the SteamID
+                
+                If the automatic override of the dll fails, you will be
+                instructed how to manually do it.
+                
+                Run the game, set the Effects and Textures search paths in the ReShade settings.
+            
+        Uninstalling ReShade for a game:
+            Run this script.
+            
+            Type u to uninstall ReShade.
+            
+            Supply the game path where the .exe file is (see instructions above).
+            
+            Supply the SteamID for the game (see instructions above).
+            
+        Removing ReShade / shader files:
+            By default the files are stored in ~/.reshade
+            Run: rm -rf ~/.reshade
+            
 DESCRIPTION
+
 function printErr() {
     echo -e "Error: $1\nExiting."
     [[ -z $2 ]] && exit 1 || exit $2
 }
-MAIN_PATH=${MAIN_PATH:-~/.reshade}
-CUSTOM_OVERRIDE=${CUSTOM_OVERRIDE:-}
-CUSTOM_OVERRIDE=$(echo "$CUSTOM_OVERRIDE" | sed "s/\.dll$//")
-if [[ ! -z $CUSTOM_OVERRIDE ]] && [[ ! $CUSTOM_OVERRIDE =~ ^(opengl32|d3d11|dinput8|ddraw|d3d8)$ ]]; then
-    echo "You have entered '$CUSTOM_OVERRIDE' as the CUSTOM_OVERRIDE, is this correct?"
-    read -p '(y/n): ' ynCheck
-    if ! [[ $ynCheck =~ ^(y|Y|yes|YES)$ ]]; then
-        printErr "Wrong CUSTOM_OVERRIDE"
+
+function checkStdin() {
+    while true; do
+        read -p "$1" userInput
+        if [[ $userInput =~ $2 ]]; then
+            break
+        fi
+    done
+    echo "$userInput"
+}
+
+function getGamePath() {
+    echo 'Supply the folder path where the main executable (exe file) for the game is.'
+    echo 'On default steam settings, look in ~/.local/share/Steam/steamapps/common/'
+    echo '(Control+c to exit)'
+    while true; do
+        gamePath=
+        read -p 'Game path: ' gamePath
+        
+        gamePath="$(realpath "$gamePath")" 2> /dev/null
+        
+        ls "$gamePath" > /dev/null 2>&1
+        if [[ $? != 0 ]] || [[ -z $gamePath ]]; then
+            echo "Incorrect or empty path supplied. You supplied \"$gamePath\"."
+            continue
+        fi
+        
+        ls "$gamePath/"*.exe > /dev/null 2>&1
+        if [[ $? != 0 ]]; then
+            echo "No .exe file found in \"$gamePath\"."
+            echo "Do you still want to use this directory?"
+            if [[ $(checkStdin "(y/n) " "^(y|n)$") != "y" ]]; then
+                continue
+            fi
+        fi
+        
+        echo "Is this path correct? \"$gamePath\""
+        if [[ $(checkStdin "(y/n) " "^(y|n)$") == "y" ]]; then
+            break
+        fi
+    done
+}
+
+function getSteamID() {
+    echo 'Please supply the SteamID of the game (To find the SteamID, run: protontricks -s Name_Of_Game). (Control+c to exit)'
+    SteamID=$(checkStdin "SteamID: " "^[0-9]*$")
+}
+
+function checkUserReg() {
+    regFile=~/".local/share/Steam/steamapps/compatdata/$SteamID/pfx/user.reg"
+    if [[ ! -f $regFile ]]; then
+        regFile=
+        echo "Could not modify or find user.reg file: \"$regFile\""
+        echo "Manually run: protontricks $SteamID winecfg"
+        echo "In the Libraries tab, $1."
+        read -p 'Press any key to continue.'
     fi
-fi
-UPDATE_RESHADE=${UPDATE_RESHADE:-1}
-D3DCOMPILER=${D3DCOMPILER:-1}
-RESHADE_PATH="$MAIN_PATH/reshade"
+}
+
 SEPERATOR="------------------------------------------------------------------------------------------------"
-echo "ReShade installer/updater for Steam and proton on Linux."
+
+OVERRIDE_REGEX='"OVERRIDE"="native,builtin"'
+COMMON_OVERRIDES="d3d8 d3d9 d3d11 ddraw dinput8 dxgi opengl32"
+
+echo -e "$SEPERATOR\nReShade installer/updater for Steam and proton on Linux.\n$SEPERATOR\n"
+
+MAIN_PATH=${MAIN_PATH:-~/".reshade test"}
+RESHADE_PATH="$MAIN_PATH/reshade"
+
 mkdir -p "$MAIN_PATH" || printErr "Unable to create directory '$MAIN_PATH'."
 cd "$MAIN_PATH"
+
+UPDATE_RESHADE=${UPDATE_RESHADE:-1}
+D3DCOMPILER=${D3DCOMPILER:-1}
+
+DLLOVERRIDE=${DLLOVERRIDE:-}
+DLLOVERRIDE=$(echo "$DLLOVERRIDE" | sed "s/.dll//")
+if [[ ! -z $DLLOVERRIDE ]] && [[ ! $DLLOVERRIDE =~ $(echo $COMMON_OVERRIDES | sed 's/ /|/g' | sed 's/^/^(/' | sed 's/$/)$/') ]]; then
+    echo "You have entered '$DLLOVERRIDE' as the DLLOVERRIDE, is this correct?"
+    read -p '(y/n): ' ynCheck
+    if ! [[ $ynCheck =~ ^(y|Y|yes|YES)$ ]]; then
+        printErr "Wrong DLLOVERRIDE"
+    fi
+fi
+
+echo "Do you want to (i)nstall or (u)ninstall ReShade for a game?"
+if [[ $(checkStdin "(i/u): " "^(i|u)$") == "u" ]]; then
+    getGamePath
+    getSteamID
+    echo "Unlinking ReShade files."
+    LINKS="$(echo $COMMON_OVERRIDES | sed 's/ /.dll /g' | sed 's/$/.dll/') ReShade32.json ReShade64.json Shaders Textures"
+    for link in $LINKS; do
+        if [[ -L $gamePath/$link ]]; then
+            echo "Unlinking \"$gamePath/$link\"."
+            unlink "$gamePath/$link"
+        fi
+    done
+    
+    echo "Removing dll overrides."
+    checkUserReg "remove overrides for $(echo $COMMON_OVERRIDES | sed 's/ / ,/g')"
+    if [[ -f $regFile ]]; then
+        for override in $COMMON_OVERRIDES; do
+            pattern=$(echo $OVERRIDE_REGEX | sed "s/OVERRIDE/$override/")
+            if [[ $(grep -Po $pattern $regFile) != "" ]]; then
+                pattern="s/$pattern\n//g"
+                echo "Removing dll override (sed -zi '$pattern' \"$regFile\")."
+                sed -zi $pattern "$regFile"
+            fi
+        done
+    fi
+    echo "Finished uninstalling ReShade for SteamID $SteamID."
+    exit 0
+fi
+
 if [[ ! -d reshade-shaders ]]; then
     echo -e "Installing reshade shaders.\n$SEPERATOR"
     git clone --branch master https://github.com/crosire/reshade-shaders || printErr "Unable to clone https://github.com/crosire/reshade-shaders"
@@ -93,23 +219,30 @@ elif [[ $UPDATE_RESHADE -eq 1 ]]; then
     git pull || printErr "Could not update ReShade shaders."
     cd "$MAIN_PATH"
 fi
+
 echo "$SEPERATOR"
 mkdir -p "$RESHADE_PATH"
+
 VERS=0
 if [[ -e VERS ]]; then
     VERS=$(cat VERS)
 fi
+
 if [[ ! -f reshade/dxgi.dll ]] || [[ $UPDATE_RESHADE -eq 1 ]]; then
     echo -e "Checking for Reshade updates.\n$SEPERATOR"
     RVERS=$(curl -s https://reshade.me | grep -Po "downloads/\S+?\.exe" || exit 1)
-    if ! [[ $? -eq 0 ]]; then printErr "Could not fetch ReShade version."; fi
+    if ! [[ $? -eq 0 ]]; then
+        printErr "Could not fetch ReShade version."
+    fi
     if [[ $RVERS != $VERS ]]; then
         echo -e "Updating Reshade."
         tmpDir=$(mktemp -d)
         cd "$tmpDir" || printErr "Failed to create temp directory."
         wget -q https://reshade.me/"$RVERS" || printErr "Could not download latest version of ReShade."
         exeFile="$(find . -name *.exe || exit 1)"
-        if ! [[ $? -eq 0 ]]; then printErr "Download of ReShade exe file failed."; fi
+        if ! [[ $? -eq 0 ]]; then
+            printErr "Download of ReShade exe file failed."
+        fi
         7z -y e "$exeFile" 1> /dev/null || printErr "Failed to extract ReShade using 7z."
         mv *32.dll d3d9.dll
         mv *64.dll dxgi.dll
@@ -121,57 +254,69 @@ if [[ ! -f reshade/dxgi.dll ]] || [[ $UPDATE_RESHADE -eq 1 ]]; then
         rm -rf "$tmpDir"
     fi
 fi
-echo -e "$SEPERATOR\nInstalling d3dcompiler_47 and setting dxgi override. Make sure to run the game at least once for steam to create the appropriate directories.\n$SEPERATOR"
-echo 'Supply the steamid of the game to install d3dcompiler_47 (To find the steamid, run: protontricks -s Name_Of_Game). (Control+c to exit)'
-echo "Enter n to skip this if d3dcompiler_47 is already installed and dxgi override is already set."
-while true; do
-    read -p 'steamid: ' steamid
-    if [[ $steamid =~ ^([0-9]*|n|no|N|NO)$ ]]; then
-        break
-    fi
-done
-echo "$SEPERATOR"
-if [[ $steamid =~ ^[0-9]*$ ]]; then
-    if [[ $D3DCOMPILER -eq 1 ]]; then
-        protontricks $steamid d3dcompiler_47
-    fi
-    regFile=~/".local/share/Steam/steamapps/compatdata/$steamid/pfx/user.reg"
-    if [[ -z $CUSTOM_OVERRIDE ]]; then
-        if [[ -e $regFile ]] && [[ $(grep -Po '^"dxgi"="native,builtin"' $regFile) == "" ]]; then
-            echo "Adding dll overrides for d3d9 and dxgi."
-            sed -i 's/^"\*d3dcompiler_47"="native"/\0\n"dxgi"="native,builtin"\n"d3d9"="native,builtin"/' "$regFile"
+
+getGamePath
+
+echo "Do you want $0 to attempt to automatically detect the right dll to use for ReShade?"
+
+wantedDll="manual"
+if [[ $(checkStdin "(y/n) " "^(y|n)$") == "y" ]]; then
+    wantedDll="auto"
+fi
+
+if [[ $wantedDll == "auto" ]]; then
+    exeArch=32
+    for file in "$gamePath/"*.exe; do
+        if [[ $(file $file) =~ x86-64 ]]; then
+            exeArch=64
         fi
-        if [[ ! -e $regFile ]] || [[ $(grep -Po '^"dxgi"="native,builtin"' $regFile) == "" ]]; then
-            echo -e "Could not modify or find user.reg file: \"$regFile\"\nManually run: protontricks $steamid winecfg\nIn the Libraries tab, Add dxgi.dll and d3d9.dll and make sure they are set to \"native,builtin\"."
-            read -p 'Press any key to continue.'
-        fi
-    fi
-    if [[ ! -z $CUSTOM_OVERRIDE ]] && [[ -e $regFile ]] && [[ $(grep -Po "^\"$CUSTOM_OVERRIDE\"=\"native,builtin\"" $regFile) == "" ]]; then
-        echo "Adding dll override for $CUSTOM_OVERRIDE."
-        sed -i "s/^\"\*d3dcompiler_47\"=\"native\"/\0\n\"$CUSTOM_OVERRIDE\"=\"native,builtin\"/" "$regFile"
+    done
+    [[ $exeArch -eq 32 ]] && wantedDll="d3d9" || wantedDll="dxgi"
+    echo "We have detected the game is $exeArch bits, we will use $wantedDll.dll as the override, is this correct?"
+    if [[ $(checkStdin "(y/n) " "^(y|n)$") == "n" ]]; then
+        wantedDll="manual"
     fi
 fi
-echo -e "Installing reshade to game directory.\n$SEPERATOR"
-echo 'Supply the folder path where the main executable (exe file) for the game is. (On default steam settings, look in ~/.local/share/Steam/steamapps/common/) (Control+c to exit)'
-while true; do
-    read -p 'Game path: ' gamePath
-    ls "$gamePath" > /dev/null 2>&1
-    if [[ $? != 0 ]] || [[ -z $gamePath ]]; then
-        echo "Incorrect or empty path supplied."
-        continue
-    fi
-    echo "Is this correct? \"$gamePath\""
-    read -p '(y/n): ' ynCheck
-    if [[ $ynCheck =~ ^(y|Y|yes|YES)$ ]]; then
-        break
-    fi
-done
-gamePath="$(realpath "$gamePath")"
-if ! [[ -z $CUSTOM_OVERRIDE ]]; then
-    echo "Linking dxgi.dll as $CUSTOM_OVERRIDE.dll."
-    ln -is $(realpath ~/.reshade/reshade/dxgi.dll) "$gamePath/$CUSTOM_OVERRIDE.dll"
+
+if [[ $wantedDll == "manual" ]]; then
+    echo "Manually enter the dll override for ReShade, common values are one of: $COMMON_OVERRIDES"
+    while true; do
+        read -p 'Override: ' wantedDll
+        echo "You have entered '$wantedDll', is this correct?"
+        read -p '(y/n): ' ynCheck
+        if [[ $ynCheck =~ ^(y|Y|yes|YES)$ ]]; then
+            break
+        fi
+    done
 fi
-ln -is $(realpath ~/.reshade/reshade/*) "$gamePath/"
-ln -is $(realpath ~/.reshade/reshade-shaders/Textures) "$gamePath/"
-ln -is $(realpath ~/.reshade/reshade-shaders/Shaders) "$gamePath/"
-echo -e "$SEPERATOR\nDone.\nThe next time you start the game, open the ReShade settings, go to the 'Settings' tab, add the Shaders folder location to the 'Effect Search Paths', add the Textures folder to the 'Texture Search Paths', go to the 'Home' tab, click 'Reload'."
+
+getSteamID
+
+if [[ $D3DCOMPILER -eq 1 ]]; then
+    echo -e "$SEPERATOR\nInstalling d3dcompiler_47 using protontricks."
+    protontricks $SteamID d3dcompiler_47
+fi
+
+checkUserReg "Add $wantedDll and make sure it is set to  \"native,builtin\"."
+
+if [[ -f $regFile ]] && [[ $(grep -Po "^\"$wantedDll\"=\"native,builtin\"" $regFile) == "" ]]; then
+    echo "Adding dll override for $wantedDll."
+    sed -i "s/^\"\*d3dcompiler_47\"=\"native\"/\0\n\"$wantedDll\"=\"native,builtin\"/" "$regFile"
+fi
+
+echo "Linking ReShade files to game directory."
+
+if [[ $wantedDll == "d3d9" ]]; then
+    ln -is "$(realpath "$RESHADE_PATH"/d3d9.dll)" "$gamePath/"
+else
+    echo "Linking dxgi.dll as $wantedDll.dll."
+    ln -is "$(realpath "$RESHADE_PATH"/dxgi.dll)" "$gamePath/$wantedDll.dll"
+fi
+
+ln -is "$(realpath "$RESHADE_PATH"/ReShade32.json)" "$gamePath/"
+ln -is "$(realpath "$RESHADE_PATH"/ReShade64.json)" "$gamePath/"
+ln -is "$(realpath "$MAIN_PATH"/reshade-shaders/Textures)" "$gamePath/"
+ln -is "$(realpath "$MAIN_PATH"/reshade-shaders/Shaders)" "$gamePath/"
+
+echo -e "$SEPERATOR\nDone."
+echo "The next time you start the game, open the ReShade settings, go to the 'Settings' tab, add the Shaders folder location to the 'Effect Search Paths', add the Textures folder to the 'Texture Search Paths', go to the 'Home' tab, click 'Reload'."
